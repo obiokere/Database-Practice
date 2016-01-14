882c5517dfb57e592b9758d0a90de95ba0d21c55
obiokere <ObiOkere@us1087969lt.am-can.regus.local>
2016-01-14 16:58:43 -0500

    Upload
    upload

---
+++ geodata/where.js
@@ -0,0 +1,8 @@
+myData = [
+[40.8141777,-73.9508766, '12 W 130th St, New York, NY 10027, USA'],
+[42.340075,-71.0895367, 'Northeastern, Boston, MA 02115, USA'],
+[38.2113643,-85.7470011, 'Bradley Ave, Louisville, KY, USA'],
+[32.778949,35.019648, 'Technion/ Sports Building, Haifa'],
+[18.4574518,73.8837999, 'Vishwakarma Institutes Play Ground, Yashodhan Society, Kapil Nagar, Kondhwa Budrukh, Vishwakarma, Maharashtra 411048, India'],
+[33.1561058,131.826132, 'Japan, 〒875-0002 Ōita-ken, Usuki-shi, Shitanoe, 1232−2 ＵＭＤ']
+];
